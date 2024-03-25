7802de8715d93602f8f5e76e450ce0b270ca8c42
Tippaphanun Kungwarnkanok <tip.kus48@gmail.com>
2024-03-25 13:51:55 +0700

    Update Banner & NavBar

--- src/components/HomeBanner.tsx
+++ src/components/HomeBanner.tsx
@@ -1,11 +1,16 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 import Image from "next/image";
 
 function HomeBanner() {
   const [currentCoverIndex, setCurrentCoverIndex] = useState(0);
-  const bannerCoverSrc = ["/img/cover1.jpg", "/img/cover2.jpg", "/img/cover3.jpg", "/img/cover4.jpg"];
+  const bannerCoverSrc = [
+    "/img/cover1.jpg",
+    "/img/cover2.jpg",
+    "/img/cover3.jpg",
+    "/img/cover4.jpg",
+  ];
 
   const changeCover = () => {
     const nextIndex = (currentCoverIndex + 1) % bannerCoverSrc.length;
@@ -13,8 +18,12 @@ function HomeBanner() {
   };
 
   return (
-    <div onClick={changeCover}>
+    <div className="relative w-screen h-screen" onClick={changeCover}>
       <Image src={bannerCoverSrc[currentCoverIndex]} alt={"cover"} fill />
+      <div className="relative top-[45%] text-center z-20 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+        <h1 className="text-4xl font-bold">Welcome to our services</h1>
+        <h3 className="text-xl mt-5">Have a great time here!</h3>
+      </div>
     </div>
   );
 }
