2436930207bc41e56230a788f983523f0e5a4b40
Shama Wachanachai <6633053521@student.chula.ac.th>
2024-03-25 14:06:15 +0700

    Fix import and update credentials in authOptions

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,21 @@
 import NextAuth from "next-auth/next";
 import { AuthOptions } from "next-auth";
-import { CredentialsProvider } from "next-auth/providers/credentials";
-import userLogIn from "@/app/libs/userLogin";
+import CredentialsProvider from "next-auth/providers/credentials";
+import userLogIn from "@/app/libs/userLogIn";
 
 export const authOptions:AuthOptions = {
     providers: [
         CredentialsProvider({
             name: "Credentials",
+
             credentials: {
-                username: { label: "Email", type: "email", placeholder: "email" },
-                password: { label: "Password", type: "password" }
+                email: { label: "Email", type: "email", placeholder: "email" },
+                password: { label: "Password", type: "password", placeholder: "password" }
             },
             async authorize(credentials, req) {
-
-                if(!credentials) return null;
-
+                if (!credentials) return null;
                 const user = await userLogIn(credentials.email, credentials.password);
-        
+
                 if (user) {
                     return user
                 } else {
@@ -25,17 +24,16 @@ export const authOptions:AuthOptions = {
             }
         })
     ],
-    session: {strategy: "jwt"},
+    session : {strategy: "jwt"},
     callbacks: {
         async jwt({token, user}) {
             return {...token, ...user};
         },
         async session({session, token, user}) {
-            session.user = token as any
+            session.user = token as any;
             return session;
-        }
+        },
     }
 }
-
 const handler = NextAuth(authOptions);
 export {handler as GET, handler as POST};
