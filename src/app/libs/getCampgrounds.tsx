f6f8679f1c21fd3d994354dffdae1ce521e0e4c7
Pakarn Pahulo <pakarnpahulo2003@gmail.com>
2024-03-26 05:02:10 +0700

    Update API endpoint URLs

--- src/app/libs/getCampgrounds.tsx
+++ src/app/libs/getCampgrounds.tsx
@@ -6,13 +6,17 @@ export default async function getCampgrounds(limit?: number, page?: number) {
 
   const limitParam = limit ? `?limit=${limit}` : '';
   const pageParam = page ? `&page=${page}` : '';
+  try {
 
-  const response = await fetch(
-    `${process.env.BACKEND_URL}/api/v1/campgrounds/${limitParam}${pageParam}`,
-    { next: { tags: ["campgrounds"] } }
-  );
-  if (!response.ok) {
-    throw new Error("Failed to fetch campgrounds");
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/campgrounds/${limitParam}${pageParam}`,
+      { next: { tags: ["campgrounds"] } }
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch campgrounds");
+    }
+    return await response.json();
+  } catch(error) {
+    console.log(error);
   }
-  return await response.json();
 }
