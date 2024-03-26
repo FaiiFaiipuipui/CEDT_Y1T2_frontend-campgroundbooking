b74a2c18778da8febd6743062eb91133f32a3baf
Shama Wachanachai <6633053521@student.chula.ac.th>
2024-03-26 17:57:24 +0700

    Fix typo in userLogin import

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth/next";
 import { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import userLogIn from "@/app/libs/userLogIn";
+import userLogIn from "@/app/libs/userLogin";
 
 export const authOptions:AuthOptions = {
     providers: [
