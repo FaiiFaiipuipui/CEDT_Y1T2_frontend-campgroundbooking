475eeb93b736698cd7dcfe84b7cf7fcc0222bca7
Tippaphanun Kungwarnkanok <tip.kus48@gmail.com>
2024-03-26 10:01:18 +0700

    Update CampgroundPage

--- src/app/(campgroundinfo)/campground/page.tsx
+++ src/app/(campgroundinfo)/campground/page.tsx
@@ -1,34 +1,22 @@
-"use client";
-
 import getCampgrounds from "@/app/libs/getCampgrounds";
 import CampgroundCatalog from "@/components/CampgroundCatalog";
-import { Suspense, useEffect, useState } from "react";
+import { Suspense } from "react";
 import { LinearProgress } from "@mui/material";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
-export default function Hospital() {
-  const router = useRouter();
-  const [campgrounds, setCampgrounds] = useState(null);
-  useEffect(() => {
-    getCampgrounds(50).then(data => setCampgrounds(data));
-  }, []);
-
-  if (!campgrounds) return null;
-
-
+export default function Campground() {
+  const campgrounds = getCampgrounds(50);
   return (
     <main className="text-center p-5 mx-[8%]">
       <div className="text-4xl font-bold m-10 text-left">Campground</div>
       {/* Admin ONLY */}
       {/* {profile.data.role == "admin" ? () */}
-      <button
-        className="absolute top-[17%] right-[12%] bg-emerald-500 px-4 py-1 text-white font-medium rounded-full"
-        onClick={(e) => {
-          router.push("/campground/manage/add");
-        }}
-      >
-        Add Campground
-      </button>
+      <Link href="/campground/manage/add">
+        <button className="absolute top-[17%] right-[12%] bg-emerald-500 px-4 py-1 text-white font-medium rounded-full">
+          Add Campground
+        </button>
+      </Link>
+
       <Suspense
         fallback={
           <p>
