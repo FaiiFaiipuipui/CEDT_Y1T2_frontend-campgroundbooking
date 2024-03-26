8e8a11f68b43d99736085e3ce1c7143fbad15aa8
Pakarn Pahulo <pakarnpahulo2003@gmail.com>
2024-03-26 23:30:35 +0700

    Refactor login function to handle errors

--- src/app/libs/login.tsx
+++ src/app/libs/login.tsx
@@ -1,20 +1,26 @@
 export default async function logIn(userEmail: string, userPassword: string) {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/login`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: userEmail,
-        password: userPassword,
-      }),
-    }
-  );
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/login`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: userEmail,
+          password: userPassword,
+        }),
+      }
+    );
 
-  if (!response.ok) {
-    throw new Error("Failed to login");
+    if (!response.ok) {
+      throw new Error("Failed to login");
+    }
+
+    return await response.json();
+  } catch(error) {
+    console.log(error);
   }
-  return await response.json();
+
 }
