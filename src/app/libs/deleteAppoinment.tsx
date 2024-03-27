3eb32c2d2ee64312ce44eca8f8f72c5b7516585b
Pakarn Pahulo <pakarnpahulo2003@gmail.com>
2024-03-27 10:04:19 +0700

    Refactor deleteAppoinment function to handle errors properly

--- src/app/libs/deleteAppoinment.tsx
+++ src/app/libs/deleteAppoinment.tsx
@@ -1,19 +1,20 @@
 export default async function deleteAppoinment(id: string, token: string) {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/appoinments/${id}`,
-    {
-      method: "DELETE",
-      headers: {
-        authorization: `Bearer ${token}`,
-      },
-    }
-  );
-  console.log(response);
-  if (!response.ok) {
-    throw new Error("Failed to delete Appointment");
-  }
+  try {
 
-  const test = await response.json();
-  // console.log()
-  return test;
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/appointments/${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+      );
+      console.log(response);
+      if (!response.ok) {
+        throw new Error(`Failed to delete Appointment: ${JSON.stringify(response)}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
 }
