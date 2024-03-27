0c8d16be8950801198d6920c5e91326805a2e87c
Pakarn Pahulo <pakarnpahulo2003@gmail.com>
2024-03-27 10:59:03 +0700

    Update error message in createAppointment function

--- src/app/libs/createAppointment.tsx
+++ src/app/libs/createAppointment.tsx
@@ -20,7 +20,7 @@ export default async function createAppointment(
       );
       
       if (!response.ok) {
-        throw new Error("Failed to create campgrounds");
+        throw new Error("Failed to create appointment");
       }
 
       console.log(response.json);
