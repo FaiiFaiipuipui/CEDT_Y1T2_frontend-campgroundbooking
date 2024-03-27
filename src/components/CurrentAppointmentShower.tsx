e0b88958b59eaa77cf8bd265e26ead106285c5ba
Tippaphanun Kungwarnkanok <tip.kus48@gmail.com>
2024-03-27 13:44:56 +0700

    Comment

--- src/components/CurrentAppointmentShower.tsx
+++ src/components/CurrentAppointmentShower.tsx
@@ -1,14 +1,14 @@
-function CurrentAppointmentShower({appointment}:{appointment:Object}) {
-  const array = appointment ? Object.values(appointment) : null;
-  console.log(array);
-  const info = array ? Object.values(array[1]) : null;
-  console.log(info);
-  const campground = info ? Object.values(info[3]) : null;
-  console.log(campground);
-  return (<div className="text-lg">
-    <div>appointment date: {info[1]}</div>
-    <div>appointment campground: {campground[1]}</div>
-  </div>);
-}
+// function CurrentAppointmentShower({appointment}:{appointment:Object}) {
+//   const array = appointment ? Object.values(appointment) : null;
+//   console.log(array);
+//   const info = array ? Object.values(array[1]) : null;
+//   console.log(info);
+//   const campground = info ? Object.values(info[3]) : null;
+//   console.log(campground);
+//   return (<div className="text-lg">
+//     <div>appointment date: {info[1]}</div>
+//     <div>appointment campground: {campground[1]}</div>
+//   </div>);
+// }
 
-export default CurrentAppointmentShower;
+// export default CurrentAppointmentShower;
