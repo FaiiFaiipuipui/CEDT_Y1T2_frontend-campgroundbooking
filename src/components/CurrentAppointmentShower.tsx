82ed3484c7b04dc22da0afbe673e27cc591e8cca
Pakarn Pahulo <pakarnpahulo2003@gmail.com>
2024-03-27 13:42:43 +0700

    Refactor appointment object access

--- src/components/CurrentAppointmentShower.tsx
+++ src/components/CurrentAppointmentShower.tsx
@@ -1,13 +1,13 @@
 function CurrentAppointmentShower({appointment}:{appointment:Object}) {
-  const array = appointment ? Object.values(appointment) : null;
+  const array = appointment ? Object.entries(appointment) : null;
   console.log(array);
-  const info = array ? Object.values(array[1]) : null;
+  const info = array ? Object.entries(array[1][1]) : null;
   console.log(info);
-  const campground = info ? Object.values(info[3]) : null;
-  console.log(campground);
+  const campground = info ? Object.entries(info[3][1]) : null;
+  console.log(campground); 
   return (<div className="text-lg">
-    <div>appointment date: {info[1]}</div>
-    <div>appointment campground: {campground[1]}</div>
+    <div>appointment date: {info[1][1]}</div>
+    <div>appointment campground: {campground[1][1]}</div>
   </div>);
 }
 
